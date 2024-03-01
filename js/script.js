8a73cfe2d996702e2c220af03fd1d70a6c60f299
nlakhota <43013362+nlakhota@users.noreply.github.com>
2024-02-29 22:16:41 -0600

    moving script.js inside the js directory

---
+++ js/script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('submit').addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Retrieve the entered email
+    const emailInput = document.getElementById('email').value;
+	const FnameInput = document.getElementById('firstname').value;
+	const LnameInput = document.getElementById('lastname').value;
+
+    // Check if the checkbox is checked or not
+    const isChecked = document.getElementById('age-verification').checked;
+
+    // Prepare message to display in the popup
+    let message = `Thank you for subscribing!\nName: ${FnameInput} ${LnameInput}\nEmail: ${emailInput}\n`;
+
+    if (isChecked) {
+      message += 'You will be notified with Mercedes F1 News!';
+    } else {
+      message += 'You will not be notified with Mercedes F1 News.';
+    }
+
+    // Show popup with the message
+    alert(message);
+  });
+});
