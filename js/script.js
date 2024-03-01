e9da12c3377dd47a44c2438c591c54decac42fd2
nlakhota <43013362+nlakhota@users.noreply.github.com>
2024-03-01 00:14:49 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ function init(){
   var emailInput = document.getElementById('email');
   var FnameInput = document.getElementById('firstname');
   var LnameInput = document.getElementById('lastname');
-  const isChecked = document.getElementById('age-verification').checked;
+  var isChecked = document.getElementById('age-verification').checked;
   button.addEventListener("click", myFunction);
  
 
   function myFunction() {
-    let message = `Thank you for subscribing!\nName: ${FnameInput} ${LnameInput}\nEmail: ${emailInput}\n`;
+    message = `Thank you for subscribing!\nName: ${FnameInput} ${LnameInput}\nEmail: ${emailInput}\n`;
 
     if (isChecked) {
       message += 'You will be notified with Mercedes F1 News!';
